739d85753445c64e0034eaf3f004e8063a97a7a8
Enovision IT & Web Services <j.vd.merwe@enovision.net>
2024-06-07 10:33:26 +0200

    Update DefaultController.php for hello_there

--- command/hello/DefaultController.php
+++ command/hello/DefaultController.php
@@ -1,9 +1,14 @@
 <?php
 
-namespace App\Command\hello;
+namespace App\Command\hello_there;
 
 use Minicli\Command\CommandController;
 
+/**
+ * This is a command without Opencart, just a plain command
+ * 
+ * The command would be `hello_there`
+ **/   
 class DefaultController extends CommandController
 {
     public function handle(): void
