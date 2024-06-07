0955fadbac6ef92b383c96eacc193d626eef5ccb
Enovision IT & Web Services <j.vd.merwe@enovision.net>
2024-06-07 10:38:41 +0200

    Added information

--- command/hello/DefaultController.php
+++ command/hello/DefaultController.php
@@ -1,13 +1,20 @@
 <?php
 
-namespace App\Command\hello_there;
+namespace App\Command\hello;
 
 use Minicli\Command\CommandController;
 
 /**
  * This is a command without Opencart, just a plain command
  * 
- * The command would be `hello_there`
+ * The command would be `hello`
+ * The class namespace could also be App\Command\Hello, but
+ * the folder name is also in lowercase
+ * 
+ * IMPORTANT: When you name a command in CamelCase then it would
+ * become a command in lowercase, so SimpleProductImport would become:
+ * `simpleproductimport`.
+ * But simple_product_import would become just like that name.
  **/   
 class DefaultController extends CommandController
 {
